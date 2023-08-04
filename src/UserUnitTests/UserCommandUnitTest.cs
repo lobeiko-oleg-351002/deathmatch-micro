a9a59e9750fe21ede62bf9821226c39ad176452b
lobeiko-oleg-351002 <totalit2803@gmail.com>
2023-08-04 17:24:16 +0300

    Command and query tests

---
+++ src/UserUnitTests/UserCommandUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Users.DTO.Role;
+using Application.Users.DTO.User;
+using Application.Users.Interfaces;
+using AutoMapper;
+using deathmatch_micro.Application.Users.Commands;
+using Moq;
+
+namespace UserUnitTests;
+public class UserCommandUnitTest
+{
+    private readonly IMapper _mapper;
+    private readonly Mock<IUserService> _mockUserService;
+    public UserCommandUnitTest()
+    {
+        _mapper = MappingConfigForTests.CreateMapper();
+        _mockUserService = new Mock<IUserService>();
+    }
+
+    [Fact]
+    public void CreateUserCommand_Success()
+    {
+        var cmd = new CreateUserCommand { Name = "commandUser", Password = "1234", Email = "test@test.com", Role = new ViewRoleDTO { Name = "User"} };
+        var handler = new CreateUserCommandHandler(_mockUserService.Object, _mapper);
+
+        var result = handler.Handle(cmd, new CancellationToken());
+
+        Assert.NotNull(result);
+        _mockUserService.Verify(mock => mock.Create(_mapper.Map<CreateUserDTO>(cmd)));
+    }
+
+    [Fact]
+    public void RemoveUserCommand_Success()
+    {
+        var cmd = new RemoveUserByIdCommand(new Guid());
+        var handler = new RemoveUserCommandHandler(_mockUserService.Object);
+
+        var result = handler.Handle(cmd, new CancellationToken());
+
+        Assert.NotNull(result);
+        _mockUserService.Verify(mock => mock.Delete(cmd.Id));
+    }
+}
---
+++ src/UserUnitTests/UserQueryUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using Application.Users.Interfaces;
+using Application.Users.Queries;
+using AutoMapper;
+using deathmatch_micro.Application.Users.Queries;
+using Moq;
+
+namespace UserUnitTests;
+public class UserQueryUnitTest
+{
+    private readonly IMapper _mapper;
+    private readonly Mock<IUserService> _mockUserService;
+    private readonly Mock<IRoleService> _mockRoleService;
+    public UserQueryUnitTest()
+    {
+        _mapper = MappingConfigForTests.CreateMapper();
+        _mockUserService = new Mock<IUserService>();
+        _mockRoleService = new Mock<IRoleService>();
+    }
+
+    [Fact]
+    public void GetAllUsersQuery_Success()
+    {
+        var query = new GetAllUsersQuery();
+        var handler = new GetAllUsersQueryHandler(_mockUserService.Object);
+
+        var result = handler.Handle(query, new CancellationToken());
+
+        Assert.NotNull(result);
+        _mockUserService.Verify(mock => mock.GetAll());
+    }
+
+    [Fact]
+    public void GetAllRolesQuery_Success()
+    {
+        var query = new GetAllRolesQuery();
+        var handler = new GetAllRolesQueryHandler(_mockRoleService.Object);
+
+        var result = handler.Handle(query, new CancellationToken());
+
+        Assert.NotNull(result);
+        _mockRoleService.Verify(mock => mock.GetAll());
+    }
+}
